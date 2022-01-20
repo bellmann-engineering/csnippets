a8b061bbdeec0c6d11bb1a7e8413a8b6bf8f1fc5
bellmann-engineering <57789413+bellmann-engineering@users.noreply.github.com>
2022-01-20 14:23:49 +0100

    Create structdemo.c

---
+++ structdemo.c
@@ -0,0 +1,16 @@
+typedef struct coord
+{
+	int x;
+	int y;	
+} coord;
+
+//typedef coord c;
+
+int main()
+{
+	coord meineKoordinate;
+	meineKoordinate.x = 9;
+	meineKoordinate.y = 10;
+	
+	return 0;
+}
