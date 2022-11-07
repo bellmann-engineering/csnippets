49851700b853b1722fe37aba22da7e0e494909cc
bellmann-engineering <57789413+bellmann-engineering@users.noreply.github.com>
2022-11-07 20:16:45 +0100

    Create set.cpp

---
+++ set.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <set>
+using namespace std;
+
+int main() 
+{ 
+    // Initialize set 
+    set<int> s; 
+
+    s.insert(1); 
+    s.insert(4); 
+    s.insert(2); 
+    s.insert(5); 
+    s.insert(3); 
+
+    // iterator pointing to 
+    // position where 2 is 
+    auto pos = s.find(3); 
+
+    // prints the set elements 
+    cout << "The set elements after 3 are: "; 
+    for (auto it = pos; it != s.end(); it++) 
+        cout << *it << " "; 
+
+    return 0; 
+}
